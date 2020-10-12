6c64db156e17ddc4299fb1e0da45dc728a4b805f
mrbenino <beninbenino@gmail.com>
2020-10-12 16:13:04 +0300

    responese

--- app/src/main/java/com/mrbenino/weather/MainActivity.kt
+++ app/src/main/java/com/mrbenino/weather/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.mrbenino.weather
 
-//import com.mrbenino.weather.network.APIService
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.mrbenino.weather.model.Weather
 import com.mrbenino.weather.network.APIService
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -14,12 +16,6 @@ import retrofit2.await
 import retrofit2.awaitResponse
 import retrofit2.converter.gson.GsonConverterFactory
 
-
-//                   TO DO ☐☑
-//  ☐ получения погоды сегодня
-//  ☐ делаем UI
-//    api key = 43e63ada628a1cc7cfd7a752fd831999
-
 const val BASE_URL = "https://api.openweathermap.org"
 val TAG = "MainActivity"
 
@@ -27,13 +23,11 @@ class MainActivity : AppCompatActivity() {
     private val job = Job()
     private val scope = CoroutineScope(Dispatchers.Main+job)
 
-
     val api = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(APIService::class.java)
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,11 +39,12 @@ class MainActivity : AppCompatActivity() {
 
         scope.launch {
             val call = api.currentWeather()
-            val responese = call.awaitResponse()
+            val responese = call.await()
             Log.d(TAG, responese.toString())
-//            val data = api.currentWeather().await()
-//            Log.d(TAG, data.name)
-        }
+            textView.text = responese.name
+            val temp = responese.main.temp.toInt()
+            textView2.text = (temp - 273.15).toInt().toString()
+            textView3.text = responese.weather.getOrNull(0)?.main.toString()
         }
     }
-
+}
