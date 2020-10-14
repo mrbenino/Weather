6c32691b9bc936f090269bb4a4c78c11f429a659
mrbenino <beninbenino@gmail.com>
2020-10-14 21:21:46 +0300

    error

--- app/src/main/java/com/mrbenino/weather/MainActivity.kt
+++ app/src/main/java/com/mrbenino/weather/MainActivity.kt
@@ -1,19 +1,16 @@
 package com.mrbenino.weather
 
+import kotlinx.coroutines.*
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import com.mrbenino.weather.model.Weather
+import com.bumptech.glide.Glide
 import com.mrbenino.weather.network.APIService
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.await
-import retrofit2.awaitResponse
 import retrofit2.converter.gson.GsonConverterFactory
 
 const val BASE_URL = "https://api.openweathermap.org"
@@ -32,12 +29,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         getCurrentData()
+//        Glide.with(this)
+//            .load("https://openweathermap.org/img/wn/03d@4x.png")
+//            .into(findViewById<ImageView>(R.id.imageView2))
     }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
     private fun getCurrentData(){
 
-        scope.launch {
+       scope.launch {
             val call = api.currentWeather()
             val responese = call.await()
             Log.d(TAG, responese.toString())
@@ -45,6 +46,24 @@ class MainActivity : AppCompatActivity() {
             val temp = responese.main.temp.toInt()
             textView2.text = (temp - 273.15).toInt().toString()
             textView3.text = responese.weather.getOrNull(0)?.main.toString()
+            val icon = responese.weather.getOrNull(0)?.icon.toString()
+//            imageView.setImageDrawable(getDrawable(R.drawable.a1))
+//            imageView.elevation = 0f
+
+//            Picasso.get().load("http://openweathermap.org/img/wn/04d@4x.png").into(imageView2)
+            Log.d(TAG,icon)
+//            val pach =
+//            Log.d(TAG,pach)
+//            Picasso.get()
+//                .load(pach)
+//                .error(R.drawable.a2)
+//                .into(imageView2)
+
+
+            Glide.with(this@MainActivity)
+                .load("http://openweathermap.org/img/wn/03d@4x.png")
+                .into(imageView2)
         }
+
     }
 }
